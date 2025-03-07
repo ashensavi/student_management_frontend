4c690d5373f3d1b88f521ec1aceae14fc69e9eaa
ashensavi <ashensavinda0@gmail.com>
2025-03-07 16:32:44 +0530

    dsf

--- src/app/page/dashboard/add-student/add-student.component.ts
+++ src/app/page/dashboard/add-student/add-student.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-student',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './add-student.component.html',
   styleUrl: './add-student.component.css'
 })
 export class AddStudentComponent {
+
+  public username : string="saman";
+
+  public students = [
+    {
+      name : "Senuka",
+      address : "Bokolagama",
+      salary : 34000.00
+    },
+    {
+      name : "nisadu",
+      address : "Piliyandala",
+      salary : 34000.00
+    },
+    {
+      name : "deshika",
+      address : "Thumpane",
+      salary : 35.00
+    },
+    {
+      name : "methni",
+      address : "Aluthgama",
+      salary : 34000.00
+    }
+  ]
 
 }
